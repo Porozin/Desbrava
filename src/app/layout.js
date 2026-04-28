68485924e219c123172a1c6eaa5a9c7a5946a390
Wallyson <wallyson@example.com>
2026-04-27 23:53:20 -0300

    fix: add suppressHydrationWarning to body element in root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ import { Toaster } from 'react-hot-toast';
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>
+      <body suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster 
